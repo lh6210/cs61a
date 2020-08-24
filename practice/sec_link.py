ff28164f79eb80fe13bbe9a3850851498c3dc5c3
Hang Li <lh6210@gmail.com>
2020-08-23 22:45:20 -0600

    Aug 23 commit

--- practice/sec_link.py
+++ practice/sec_link.py
@@ -85,15 +85,86 @@ def extend_link(s, t):
     >>> extend_link(l1, four)
     [2, [1, [1, [2, [3, [4, 'empty']]]]]]
     """
-    #assert is_link(s) and is_link(t), "s and t must be linked lists"
+    assert is_link(s) and is_link(t), "s and t must be linked lists"
     if rest(s) == empty:
         return link(first(s), t)
     else:
         return link(first(s), extend_link(rest(s), t))
     
 
+def apply_to_all(f, s):
+    """f is a function with a number as its argument
+    >>> s = link(1, link(2, link(3)))
+    >>> apply_to_all(lambda x: x + 3, s)
+    [4, [5, [6, 'empty']]]
+    >>> apply_to_all(lambda x: x * 2, s)
+    [2, [4, [6, 'empty']]]
+    >>> apply_to_all(lambda x: x * x, s)
+    [1, [4, [9, 'empty']]]
+    """
+
+    assert is_link(s), "s must be a link"
+    if rest(s) == empty:
+        return link(f(first(s)))
+    else:
+        return link(f(first(s)), apply_to_all(f, rest(s)))
 
 
+def keep_if_link(f, s):
+    """ the output could be 'empty', which is not a linked list anymore.
+    That happens when s is a single-node linked list and f(first(s)) is false
+    >>> four = link(1, link(2, link(3, link(4, empty))))
+    >>> keep_if_link(lambda x: x%2 == 0, four)
+    [2, [4, 'empty']]
+    >>> nine = link(3, link(6, link(9)))
+    >>> keep_if_link(lambda x: x % 3 != 0, nine)
+    'empty'
+    >>> nine = link(4, link(5, link(9)))
+    >>> keep_if_link(lambda x: x % 3 != 0, nine)
+    [4, [5, 'empty']]
+
+    """
+
+    assert is_link(s), "s must be a link"
+    if rest(s) == empty:
+        if f(first(s)) == True:
+            return s
+        else:
+            return empty
+    else:
+        if f(first(s)) == True:
+            return link(first(s), keep_if_link(f, rest(s)))
+        else:
+            return keep_if_link(f, rest(s))
+
+
+
+def join_link(s, separator):
+    """Return a string of all elements in s separated by separator.
+    >>> four = link(1, link(2, link(3, link(4))))
+    >>> join_link(four, ", ")
+    '1, 2, 3, 4'
+    >>> join_link(four, " + ") 
+    '1 + 2 + 3 + 4'
+    """
+
+    if rest(s) == empty:
+        return str(first(s))
+    else:
+        return(str(first(s)) + separator +  join_link(rest(s), separator))
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
