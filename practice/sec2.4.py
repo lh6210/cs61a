7c8dd3c6d71067a908b9f9502e5a7a189dd9fef2
Hang Li <lh6210@gmail.com>
2020-08-27 12:37:42 -0600

    lab06 updated

--- practice/sec2.4.py
+++ practice/sec2.4.py
@@ -9,3 +9,38 @@ def make_withdraw(balance):
         balance = balance - amount
         return balance
     return withdraw
+
+
+def double_and_print(x):
+    print('***', x, '=>', 2*x, '***')
+    return 2*x
+
+s = range(3, 7)
+doubled = map(double_and_print, s)
+
+
+
+d = {'one': 1, 'two':2, 'three': 3}
+d['zero'] = 0
+
+
+counts = [1, 2, 3]
+for item in counts:
+    print(item)
+
+
+items = iter(counts)
+try:
+    while True:
+        item = next(items)
+        print(item)
+except StopIteration:
+    pass
+
+
+def evens(start, end):
+    even = start + (start % 2)
+    while even < end:
+        yield even 
+        even += 2
+
